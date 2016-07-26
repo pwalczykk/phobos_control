0b5af45d00e927e7d055898797310168fde69341
Przemek <pwalczykk@gmail.com>
2016-07-26 13:53:45 +0200

    loop rate 10

--- rover_teleop/src/uart_433_control_reciver/Main.cpp
+++ rover_teleop/src/uart_433_control_reciver/Main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
 
     UART_Rx rx("/dev/ttyACM0");
 
-    ros::Rate loop_rate(20);
+    ros::Rate loop_rate(10);
 
     while(ros::ok()){
         if(rx.ReadBuffer()){
