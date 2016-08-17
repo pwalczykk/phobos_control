2ac70cbe81cdeb86b12fbdeed165253657146893
Przemek <pwalczykk@gmail.com>
2016-08-17 19:05:09 +0200

    pub_travelled_path topics rename

--- phobos_control_station/src/pub_traveled_path/Main.cpp
+++ phobos_control_station/src/pub_traveled_path/Main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "traveled_path_publisher");
     ros::NodeHandle nh;
 
-    TraveledPathPublisher traveled_path_publisher("/rover/odometry/filtered", "/rover/path");
+    TraveledPathPublisher traveled_path_publisher("/control/localization/pose_ekf", "/control/localization/travelled_path");
 
     ros::spin();
 
