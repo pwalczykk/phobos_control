09faadffcbada9f779e092de0ea1c18990ea31d2
Przemek <pwalczykk@gmail.com>
2016-09-07 17:24:30 +0200

    float messages

--- phobos_control_station/src/uart_433_control_transceiver/PubEncodersAll.hpp
+++ phobos_control_station/src/uart_433_control_transceiver/PubEncodersAll.hpp
@@ -2,18 +2,18 @@
 #define PUBENCODERSALL_HPP_
 
 #include <ros/ros.h>
-#include <phobos_shared/EncodersAll.h>
+#include <phobos_shared/EncodersAllFloat.h>
 
 class PubEncodersAll{
     ros::NodeHandle *nh;
     ros::Publisher pub;
 public:
-    phobos_shared::EncodersAll msg;
+    phobos_shared::EncodersAllFloat msg;
 
 public:
     PubEncodersAll(ros::NodeHandle *nh, std::string topic){
         this->nh = nh;
-        this->pub = nh->advertise<phobos_shared::EncodersAll>(topic, 100);
+        this->pub = nh->advertise<phobos_shared::EncodersAllFloat>(topic, 100);
     }
 
     void Publish(){
